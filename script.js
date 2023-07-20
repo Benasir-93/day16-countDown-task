0b95606024aed0bac3b8494e1b015181e32b1b0b
Benasir-93 <135957792+Benasir-93@users.noreply.github.com>
2023-07-20 19:50:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+var start=10;
+var time=document.getElementById("timer");
+time.style.color="red";
+time.style.fontSize="60px";
+time.style.width="50%";
+time.style.textAlign="center";
+time.style.border = "black 3px solid";
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{time.innerHTML = start--;
+setTimeout(()=>{
+time.style.color="blue";
+time.style.fontSize="40px";
+time.innerHTML="HAPPY INDEPENDENCE DAY !!"},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
